bdb28794d61f3369b72149c38a87f737617893d2
Iska <vgutierrezdiban@protonmail.com>
2024-04-14 21:54:25 -0400

    Refactor dot-prod

--- matrix-multiplication.scm
+++ matrix-multiplication.scm
@@ -3,14 +3,8 @@
 (define A '((1 2 3) (4 5 6)))
 (define B '((7 8 9) (1 2 3) (4 5 6)))
 
-(define dot-prod (lambda (a b)
-  (cond
-    ((null? a) 0)
-    ((null? (cdr a)) (* (car a) (car b)))
-    (#t (+
-          (* (car a) (car b))
-          (dot-prod (cdr a) (cdr b)))))))
-    
+(define dot-prod (lambda (a b) (apply + (map * a b))))
+
 (define matrix* (lambda (A B)
   (map (lambda (row)
     (map (lambda (column)
